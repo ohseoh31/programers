ea980fbda992c5b42f0575a3762a4f632d8bcd48
ohseoh31 <ohseoh44@naver.com>
2020-06-29 09:28:53 +0900

    Update crain.py

--- crain.py
+++ crain.py
@@ -1,4 +1,9 @@
-#https://programmers.co.kr/learn/courses/30/lessons/64061?language=python3
+'''
+    https://programmers.co.kr/learn/courses/30/lessons/64061?language=python3
+    크레인 인형뽑기 게임
+    2019 카카오 개발자 겨울 인턴십
+
+'''
 
 def solution(board, moves):
     answer = 0
